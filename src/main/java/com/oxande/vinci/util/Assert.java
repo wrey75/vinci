d48182adc7885b760c8f6df44fa58d1be2ecc742
William Rey <wrey75@gmail.com>
2017-11-06 22:33:08 +0100

    Improve coverage

--- src/main/java/com/oxande/vinci/util/Assert.java
+++ src/main/java/com/oxande/vinci/util/Assert.java
@@ -1,6 +1,11 @@
 package com.oxande.vinci.util;
 
 public final class Assert {
+	
+	private Assert(){
+		// The class can NEVER be created.
+	}
+	
 	public static void notNull(Object expr, String name){
 		if( expr == null ){
 			throw new IllegalArgumentException("Argument '"+name+"' MUST NOT be null.");
